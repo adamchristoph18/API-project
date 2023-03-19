41a7117feec6a7ead1cd1a348039ba4dd4390cf8
Adam Tull <adamchristoph18@gmail.com>
2023-03-18 23:20:25 -0400

    finished spots migration and model for now

---
+++ backend/db/migrations/20230319024207-create-spot.js
@@ -0,0 +1,74 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {}; // this is super important for render
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Spots', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ownerId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      city: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      state: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      country: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      lat: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      lng: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      price: {
+        allowNull: false,
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Spots";
+    await queryInterface.dropTable(options);
+  }
+};
---
+++ backend/db/models/spot.js
@@ -0,0 +1,81 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Spot.belongsTo(models.User, { foreignKey: 'ownerId' });
+      Spot.hasMany(models.Booking, { foreignKey: 'spotId' });
+      Spot.hasMany(models.Review, { foreignKey: 'spotId' });
+      Spot.hasMany(models.SpotImage, { foreignKey: 'spotId' });
+      Spot.belongsToMany(models.User, { through: models.Booking });
+      Spot.belongsToMany(models.User, { through: models.Review });
+    }
+  }
+  Spot.init({
+    ownerId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: { model: 'Owners' },
+      onDelete: 'CASCADE'
+    },
+    address: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    city: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    state: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    country: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    lat: {
+      allowNull: false,
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true
+      }
+    },
+    lng: {
+      allowNull: false,
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true
+      }
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    description: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: true
+      }
+    },
+    price: {
+      allowNull: false,
+      type: DataTypes.DECIMAL,
+      validate: {
+        min: 0
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
