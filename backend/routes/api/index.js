70503743df40e1fd023210bf914c1e346578c69e
Adam Tull <adamchristoph18@gmail.com>
2023-03-20 17:24:51 -0400

    created router file for spots

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const spotsRouter = require('./spots.js');
 const { restoreUser } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
@@ -11,6 +12,7 @@ router.use(restoreUser); // global middleware
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+router.use('/spots', spotsRouter);
 
 router.post('/test', (req, res) => { // leave this in place for when you work on the front end
   res.json({ requestBody: req.body });
