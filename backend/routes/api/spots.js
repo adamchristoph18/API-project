1f24e499451b92811e229b5bb80dba4a73e9dc24
Adam Tull <adamchristoph18@gmail.com>
2023-03-20 18:03:43 -0400

    added avgRating property to get all spots route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Spot } = require('../../db/models');
+const { Spot, Review } = require('../../db/models');
 
 const router = express.Router();
 
@@ -7,6 +7,22 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     const spotsObj = {}; // initialize a new object
     const spotsArray = await Spot.findAll(); // to get an array of spot objects
+
+    for (let spot of spotsArray) {
+
+        const reviews = await Review.findAll({
+            where: {
+                spotId: spot.id // find all of the reviews that correspond to the spot
+            }
+        });
+
+        let totalStars = 0;
+        for (let review of reviews) {
+            totalStars += review.stars;
+        }
+        spot.avgRating = totalStars / reviews.length;
+        // total of all of the review.stars / divided by reviews.length
+    }
 
     spotsObj['Spots'] = spotsArray; // set a key of 'Spots' to the array of spots
 
