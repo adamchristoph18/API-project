e84dbf1d37779ee0b6516f463d2874ed6152f651
Adam Tull <adamchristoph18@gmail.com>
2023-03-20 17:36:33 -0400

    restructured the get all spots route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,8 +5,12 @@ const router = express.Router();
 
 
 router.get('/', async (req, res) => {
-    const spots = await Spot.findAll();
-    return res.status(200).json(spots);
+    const spotsObj = {}; // initialize a new object
+    const spotsArray = await Spot.findAll(); // to get an array of spot objects
+
+    spotsObj['Spots'] = spotsArray; // set a key of 'Spots' to the array of spots
+
+    return res.status(200).json(spotsObj);
 });
 
 
