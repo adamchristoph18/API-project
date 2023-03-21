78f61c956858b2c6d27a1fde65da2866034cdfb2
Adam Tull <adamchristoph18@gmail.com>
2023-03-21 15:35:42 -0400

    finished my create a spot POST route, and created a custom validator to check the response body

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const { check } = require('express-validator');
+const { handleValidationErrorsSpots } = require('../../utils/validation');
+const { requireAuth } = require('../../utils/auth');
 const { Spot, Review, SpotImage, sequelize, User } = require('../../db/models');
 
 const router = express.Router();
@@ -33,7 +36,7 @@ router.get('/', async (req, res) => {
 
 
 // Get all Spots owned by the Current User
-router.get('/current', async (req, res) => {
+router.get('/current', requireAuth, async (req, res) => {
     const currentUserSpotsObj = {}; // initialize a new object
     const currentUserId = req.user.id;
     const currentUser = await User.findByPk(currentUserId);
@@ -101,8 +104,39 @@ router.get('/:spotId', async (req, res) => {
     return res.status(200).json(spot);
 })
 
+const validateCreateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('City is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State is required'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .withMessage('Country is required'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .exists({ checkFalsy: true })
+        .withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .withMessage('Price per day is required'),
+    handleValidationErrorsSpots
+];
+
 // Create a Spot
-router.post('/', async (req, res) => {
+router.post('/', requireAuth, validateCreateSpot, async (req, res) => {
     const currentUser = req.user;
     const { address, city, state, country, lat, lng, name, description, price } = req.body;
 
