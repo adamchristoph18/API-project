7c6c8bc0ede2554dc4cc3cce62b40da09620d0aa
Adam Tull <adamchristoph18@gmail.com>
2023-03-21 11:17:16 -0400

    added functionality to the Get All Spots owned by the Current User route to include avgRating and previewImage properties

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -38,7 +38,26 @@ router.get('/current', async (req, res) => {
     const currentUserId = req.user.id;
     const currentUser = await User.findByPk(currentUserId);
 
-    const spotsArray = await currentUser.getSpots();
+    const spotsArray = await currentUser.getSpots({
+        include: [{
+            model: Review, // this is imported above
+            as: 'Reviews',
+            attributes: [] // need this because I don't want to include any attributes from the review model
+        },
+        {
+            model: SpotImage, // this is imported above
+            as: 'SpotImages',
+            attributes: [] // need this because I don't want to include any attributes from the SpotImage model
+        }],
+
+        attributes: { // attributes to include in my query/response
+            include: [ // include here to add new columns on top of everything that spot has
+            [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgRating'],
+            [sequelize.col('SpotImages.url'), 'previewImage'] // .url is a string
+            ]
+        },
+        group: ['Spot.id', 'SpotImages.url'] // tells sql how I want the data split up (per spot)
+    });
 
     currentUserSpotsObj['Spots'] = spotsArray; // set a key of 'Spots' to the array of spots owned by the current user
     return res.status(200).json(currentUserSpotsObj);
