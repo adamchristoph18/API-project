e7c030f724e2dbe3682794e9afb7f4e141851283
Adam Tull <adamchristoph18@gmail.com>
2023-04-10 16:26:08 -0400

    declared state variables in new spot form

--- frontend/src/components/CreateNewSpotForm/CreateNewSpotForm.js
+++ frontend/src/components/CreateNewSpotForm/CreateNewSpotForm.js
@@ -1,6 +1,15 @@
-
+import { useEffect, useState } from "react";
 
 function CreateNewSpotForm() {
+    const [country, setCountry] = useState('');
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [description, setDescription] = useState('');
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState(0);
+    const [errors, setErrors] = useState({});
+
     return (
         <div>Hello!</div>
     )
