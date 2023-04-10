3a99025b1f0f22c1d13e48085fe5b0d806fc9615
Adam Tull <adamchristoph18@gmail.com>
2023-04-10 16:59:00 -0400

    finished styling top of form for create new spot

--- frontend/src/components/CreateNewSpotForm/CreateNewSpotForm.js
+++ frontend/src/components/CreateNewSpotForm/CreateNewSpotForm.js
@@ -20,31 +20,38 @@ function CreateNewSpotForm() {
         <form
             className="new-spot-form"
         >
-            <h2>Create a new Spot</h2>
-            <label>
-                Country
-                <br/>
-                <input
-                    type="text"
-                    name="country"
-                    placeholder="Country"
-                    value={country}
-                    onChange={(e) => setCountry(e.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Street Address
-                <br/>
-                <input
-                    type="text"
-                    name="address"
-                    placeholder="Street Address"
-                    value={country}
-                    onChange={(e) => setCountry(e.target.value)}
-                    required
-                />
-            </label>
+            <div>
+                <h2 className="form-title">Create a new Spot</h2>
+                <p><span className="subtitle">Where's your place located?</span>
+                    <br/>
+                    <span className="disclaim">Guests will only get your exact address once they have booked a reservation.</span></p>
+            </div>
+            <div className="form-info">
+                <label>
+                    Country
+                    <br/>
+                    <input
+                        type="text"
+                        name="country"
+                        placeholder="Country"
+                        value={country}
+                        onChange={(e) => setCountry(e.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Street Address
+                    <br/>
+                    <input
+                        type="text"
+                        name="address"
+                        placeholder="Street Address"
+                        value={address}
+                        onChange={(e) => setAddress(e.target.value)}
+                        required
+                    />
+                </label>
+            </div>
         </form>
     )
 }
