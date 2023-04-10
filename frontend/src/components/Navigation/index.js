1a773080a28d320607ec7f0b9fcc2d83cca86b26
Adam Tull <adamchristoph18@gmail.com>
2023-04-09 23:10:56 -0400

    added a link to creat a new spot to my nav bar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -10,21 +10,26 @@ function Navigation({ isLoaded }){
   return (
     <div className='nav-bar'>
       <NavLink className='short-havens-title' exact to="/">Short Havens</NavLink>
-        <div className='profile-options-container'>
-          <div className='profile-nav'>
-            {/* <a>
-              <NavLink exact to="/">Home</NavLink>
-            </a> */}
-            {isLoaded && (
-              <>
-                <i className="fa-solid fa-bars icon icon-large clickable" />
-                <a>
-                  <ProfileButton user={sessionUser} />
-                </a>
-              </>
-            )}
+
+        <div className='nav-bar-right'>
+
+          <NavLink className='create-new-spot' to="/">Create a New Spot</NavLink>
+          <div className='profile-options-container'>
+            <div className='profile-nav'>
+              {isLoaded && (
+                <>
+                  <i className="fa-solid fa-bars icon icon-large clickable" />
+                  <a>
+                    <ProfileButton user={sessionUser} />
+                  </a>
+                </>
+              )}
+            </div>
           </div>
-          </div>
+
+
+        </div>
+
     </div>
   );
 }
