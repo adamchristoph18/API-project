901e4d8800394668170186645a2675ddea501231
Adam Tull <adamchristoph18@gmail.com>
2023-04-10 19:01:16 -0400

    finished the images section of my create a spot form

--- frontend/src/components/CreateNewSpotForm/CreateNewSpotForm.js
+++ frontend/src/components/CreateNewSpotForm/CreateNewSpotForm.js
@@ -11,6 +11,10 @@ function CreateNewSpotForm() {
     const [longitude, setLongitude] = useState(0);
     const [title, setTitle] = useState('');
     const [price, setPrice] = useState(0);
+    const [prevImgUrl, setPrevImgUrl] = useState('');
+    const [prevImgUrlTwo, setPrevImgUrlTwo] = useState(''); // these aren't required
+    const [prevImgUrlThree, setPrevImgUrlThree] = useState(''); // these aren't required
+    const [prevImgUrlFour, setPrevImgUrlFour] = useState(''); // turn these into an array?
     const [errors, setErrors] = useState({});
 
     useEffect(() => {
@@ -123,11 +127,77 @@ function CreateNewSpotForm() {
                         onChange={(e) => setDescription(e.target.value)}
                     />
                 </div>
-                <div>
-                    <p><span className="describe-title">Create a title for your spot</span>
+                <div className="title-div">
+                    <p><span className="spot-title-title">Create a title for your spot</span>
                         <br/>
-                        <span className="describe-tips">Catch guests' attention with a spot title that highlights
+                        <span className="title-tips">Catch guests' attention with a spot title that highlights
                             what makes your place special.</span></p>
+                    <input
+                        className="title-input"
+                        type="text"
+                        name="title"
+                        placeholder="Name of your spot"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                        />
+                </div>
+                <div className="title-div">
+                    <p><span className="price-title">Set a base price for your spot</span>
+                        <br/>
+                        <span className="price-tips">Competitive pricing can help your listing stand out and rank
+                            higher in search results.</span></p>
+                    <div className="price-line">
+                        <i className="fa-solid fa-dollar-sign icon dolla" />
+                            <input
+                                className="price-input"
+                                type="number"
+                                name="price"
+                                placeholder="Price per night (USD)"
+                                value={price}
+                                onChange={(e) => setPrice(e.target.value)}
+                                required
+                                />
+                        <p className="per-night">/ per night (USD)</p>
+                    </div>
+                </div>
+                <div className="photos-div">
+                    <p><span className="photo-title">Liven up your spot with photos!</span>
+                        <br/>
+                        <span className="photo-tips">Submit a link to at least one photo to publish your spot.</span></p>
+                        <input
+                            className="image-input"
+                            type="text"
+                            name="image-url-one"
+                            placeholder="Preview Image URL"
+                            value={prevImgUrl}
+                            onChange={(e) => setPrevImgUrl(e.target.value)}
+                            required
+                        />
+                        <input
+                            className="image-input"
+                            type="text"
+                            name="image-url-two"
+                            placeholder="Image URL"
+                            value={prevImgUrlTwo}
+                            onChange={(e) => setPrevImgUrlTwo(e.target.value)}
+                        />
+                        <input
+                            className="image-input"
+                            type="text"
+                            name="image-url-three"
+                            placeholder="Image URL"
+                            value={prevImgUrlThree}
+                            onChange={(e) => setPrevImgUrlThree(e.target.value)}
+                        />
+                        <input
+                            className="last-image-input"
+                            type="text"
+                            name="image-url-four"
+                            placeholder="Image URL"
+                            value={prevImgUrlFour}
+                            onChange={(e) => setPrevImgUrlFour(e.target.value)}
+                        />
                 </div>
             </div>
         </form>
