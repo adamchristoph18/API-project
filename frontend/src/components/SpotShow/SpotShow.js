74e08b967992b5dc2b041ae81abfb4388bd8afce
Adam Tull <adamchristoph18@gmail.com>
2023-04-11 19:02:21 -0400

    added functionality to render preview image on the spot details page

--- frontend/src/components/SpotShow/SpotShow.js
+++ frontend/src/components/SpotShow/SpotShow.js
@@ -1,17 +1,35 @@
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { displaySpotThunk } from '../../store/spots';
 import { useEffect } from 'react';
+import './SpotShow.css';
 
 
 const SpotShow = () => {
     const { spotId } = useParams();
-    const spot = useSelector(state => state.spots.allSpots[spotId]);
+    const spotObj = useSelector(state => state.spots.singleSpot);
 
+    const imagesArr = spotObj.SpotImages;
+    // console.log('tis is the imgs ----------> ', imagesArr[0]);
     const dispatch = useDispatch();
 
-    // useEffect(() => {
-    //     dispatch()
-    // }, [dispatch, spotId]);
+    useEffect(() => {
+        dispatch(displaySpotThunk(spotId));
+    }, [dispatch, spotId]);
+
+    return (
+        <div className='spot-details-page'>
+            <div className='spot-details-header'>
+                <h1>{spotObj.name}</h1>
+                    <p>{spotObj.city}, {spotObj.state}, {spotObj.country}</p>
+            </div>
+            <div className='spot-images'>
+                <img src={imagesArr[0].url} />
+            </div>
+
+        </div>
+    )
+
 }
 
 export default SpotShow;
