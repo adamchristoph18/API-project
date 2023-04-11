d5062b3cb619e6b4a11d50bfc4faede0ed2b8350
Adam Tull <adamchristoph18@gmail.com>
2023-04-11 14:08:51 -0400

    set up correct imports for spot show component file

--- frontend/src/components/SpotShow/SpotShow.js
+++ frontend/src/components/SpotShow/SpotShow.js
@@ -1,7 +1,17 @@
 import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
 
 const SpotShow = () => {
+    const { spotId } = useParams();
+    const spot = useSelector(state => state.spots.allSpots[spotId]);
 
+    const dispatch = useDispatch();
+
+    // useEffect(() => {
+    //     dispatch()
+    // }, [dispatch, spotId]);
 }
 
 export default SpotShow;
