45a1fb03f4cf613c4ca3473ebcdbcc9af8993292
Adam Tull <adamchristoph18@gmail.com>
2023-04-11 20:54:53 -0400

    added checks on my show spot details component to force rerender

--- frontend/src/components/SpotShow/SpotShow.js
+++ frontend/src/components/SpotShow/SpotShow.js
@@ -8,23 +8,64 @@ import './SpotShow.css';
 const SpotShow = () => {
     const { spotId } = useParams();
     const spotObj = useSelector(state => state.spots.singleSpot);
-
     const imagesArr = spotObj.SpotImages;
-    // console.log('tis is the imgs ----------> ', imagesArr[0]);
+
+    console.log('first image ----------> ', spotId);
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(displaySpotThunk(spotId));
     }, [dispatch, spotId]);
 
+    if (!spotObj) return null;
+    if (!imagesArr) return null;
+
     return (
         <div className='spot-details-page'>
-            <div className='spot-details-header'>
+            <div>
                 <h1>{spotObj.name}</h1>
                     <p>{spotObj.city}, {spotObj.state}, {spotObj.country}</p>
             </div>
             <div className='spot-images'>
-                <img src={imagesArr[0].url} />
+                <img
+                    className='preview-image'
+                    src={imagesArr[0].url}
+                    alt="spot's preview image"
+                />
+                <div className='extra-images'>
+                    {imagesArr.length > 1 ?
+                                        <img
+                                            className='extra-image x-image-one'
+                                            src={imagesArr[1].url}
+                                            alt="spot image"
+                                        />
+                                        : null
+                                        }
+                    {imagesArr.length > 2 ?
+                                        <img
+                                            className='extra-image x-image-two'
+                                            src={imagesArr[2].url}
+                                            alt="spot image"
+                                        />
+                                        : null
+                                        }
+                    {imagesArr.length > 3 ?
+                                        <img
+                                            className='extra-image x-image-three'
+                                            src={imagesArr[3].url}
+                                            alt="spot image"
+                                        />
+                                        : null
+                                        }
+                    {imagesArr.length > 4 ?
+                                        <img
+                                            className='extra-image x-image-four'
+                                            src={imagesArr[4].url}
+                                            alt="spot image"
+                                        />
+                                        : null
+                                        }
+                </div>
             </div>
 
         </div>
