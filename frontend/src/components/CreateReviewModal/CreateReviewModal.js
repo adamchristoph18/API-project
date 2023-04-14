3d083cbd6f10f18288a586aaf791ae4b9a7a1aa2
Adam Tull <adamchristoph18@gmail.com>
2023-04-14 17:26:57 -0400

    finished functionality and styling for star rating when creating a review

--- frontend/src/components/CreateReviewModal/CreateReviewModal.js
+++ frontend/src/components/CreateReviewModal/CreateReviewModal.js
@@ -1,9 +1,12 @@
 import { useModal } from "../../context/Modal";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import "./CreateReviewModal.css";
 // import thunk here
 
 function CreateReviewModal() {
+    const [rating, setRating] = useState(1);
+    const [activeRating, setActiveRating] = useState(1);
     const { closeModal } = useModal();
 
     const dispatch = useDispatch();
@@ -15,6 +18,86 @@ function CreateReviewModal() {
                 className="new-review-text"
                 placeholder="Just a quick review"
             />
+
+
+
+            <div className="star-average-rating-input">
+                <div
+                    className={activeRating >= 1 ? "star-filled" : "star-empty"}
+                    onMouseEnter={() => {
+                        setActiveRating(1)
+                    }}
+                    onMouseLeave={() => {
+                        setActiveRating(activeRating)
+                    }}
+                    onClick={() => {
+                        setRating(1);
+                    }}
+                >
+                    <i className="fa-solid fa-star medium-big-star clickable" />
+                </div>
+                <div
+                    className={activeRating >= 2 ? "star-filled" : "star-empty"}
+                    onMouseEnter={() => {
+                        setActiveRating(2)
+                    }}
+                    onMouseLeave={() => {
+                        setActiveRating(activeRating)
+                    }}
+                    onClick={() => {
+                        setRating(2);
+                    }}
+                >
+                    <i className="fa-solid fa-star medium-big-star clickable" />
+                </div>
+                <div
+                    className={activeRating >= 3 ? "star-filled" : "star-empty"}
+                    onMouseEnter={() => {
+                        setActiveRating(3)
+                    }}
+                    onMouseLeave={() => {
+                        setActiveRating(activeRating)
+                    }}
+                    onClick={() => {
+                        setRating(3);
+                    }}
+                >
+                    <i className="fa-solid fa-star medium-big-star clickable" />
+                </div>
+                <div
+                    className={activeRating >= 4 ? "star-filled" : "star-empty"}
+                    onMouseEnter={() => {
+                        setActiveRating(4)
+                    }}
+                    onMouseLeave={() => {
+                        setActiveRating(activeRating)
+                    }}
+                    onClick={() => {
+                        setRating(4);
+                    }}
+                >
+                    <i className="fa-solid fa-star medium-big-star clickable" />
+                </div>
+                <div
+                    className={activeRating >= 5 ? "star-filled" : "star-empty"}
+                    onMouseEnter={() => {
+                        setActiveRating(5)
+                    }}
+                    onMouseLeave={() => {
+                        setActiveRating(activeRating)
+                    }}
+                    onClick={() => {
+                        setRating(5);
+                    }}
+                >
+                    <i className="fa-solid fa-star medium-big-star clickable" />
+                </div>
+            </div>
+
+
+
+
+
         </div>
     )
 }
