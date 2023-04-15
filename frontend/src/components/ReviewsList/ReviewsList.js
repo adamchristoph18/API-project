458e92c04a3b85fd20e48ef90b258c23f5cc1555
Adam Tull <adamchristoph18@gmail.com>
2023-04-15 16:38:33 -0400

    added styling for each individual review

--- frontend/src/components/ReviewsList/ReviewsList.js
+++ frontend/src/components/ReviewsList/ReviewsList.js
@@ -1,16 +1,33 @@
+import "./ReviewsList.css";
 
 const ReviewsList = ({ review }) => {
-    // const dateCreated = () => new Date(review.createdAt);
-    // const month = dateCreated.getMonth();
+    const dateCreated = new Date(review.createdAt);
+    const month = dateCreated.getMonth();
+    const year = dateCreated.getFullYear();
+
+    const months = {
+        0: "January",
+        1: "February",
+        2: "March",
+        3: "April",
+        4: "May",
+        5: "June",
+        6: "July",
+        7: "August",
+        8: "September",
+        9: "October",
+        10: "November",
+        11: "December"
+    };
 
     return (
-        <div>
-            <h3>
+        <div className="each-review">
+            <h3 className="review-author">
                 {review.User?.firstName}
             </h3>
-            {/* <p>
-                {month}
-            </p> */}
+            <p className="date-of-review">
+                {months[month]}, {year}
+            </p>
             <p>
                 {review.review}
             </p>
