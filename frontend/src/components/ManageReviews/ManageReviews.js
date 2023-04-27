081dadced3aed8c10afbcf21691f638e9dca0685
Adam Tull <adamchristoph18@gmail.com>
2023-04-27 18:12:44 -0400

    added import for getting current users reviews from the backend and set up dispatch

--- frontend/src/components/ManageReviews/ManageReviews.js
+++ frontend/src/components/ManageReviews/ManageReviews.js
@@ -1,11 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import getCurrentUsersReviewsThunk from "../../store/reviews";
 import ReviewsList from "../ReviewsList/ReviewsList"
 import "./ManageReviews.css";
+import { useEffect } from "react";
 
 const ManageReviews = () => {
+    const reviewsObj = useSelector(state => state.reviews.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCurrentUsersReviewsThunk());
+    }, [dispatch]);
+
+
+    if (!reviewsObj) return null;
+
     return (
         <div>
             <h1>Manage Reviews</h1>
-            {/* <ReviewsList /> */}
         </div>
     )
 };
