16de9fb8ed716bbf7ce21b654f25ff9fb887c845
Adam Tull <adamchristoph18@gmail.com>
2023-04-28 03:09:29 -0400

    mapped over reviews array and rendered reviewsList component

--- frontend/src/components/ManageReviews/ManageReviews.js
+++ frontend/src/components/ManageReviews/ManageReviews.js
@@ -1,11 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
-import {getCurrentUsersReviewsThunk} from "../../store/reviews";
+import { getCurrentUsersReviewsThunk } from "../../store/reviews";
 import ReviewsList from "../ReviewsList/ReviewsList"
 import "./ManageReviews.css";
 import { useEffect } from "react";
 
 const ManageReviews = () => {
     const reviewsObj = useSelector(state => state.reviews.user);
+    const reviewsArr = Object.values(reviewsObj);
+
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -18,6 +20,12 @@ const ManageReviews = () => {
     return (
         <div>
             <h1>Manage Reviews</h1>
+            {reviewsArr.map(review => (
+                <ReviewsList
+                    review={review}
+                    key={review.id}
+                />
+            ))}
         </div>
     )
 };
