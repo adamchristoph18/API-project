55cf00712178f5a2336ffb19b74571cc3e90142b
Adam Tull <adamchristoph18@gmail.com>
2023-06-12 15:11:32 -0400

    finished basic styling for the back to homepage button on the manage reviews component

--- frontend/src/components/ManageReviews/ManageReviews.js
+++ frontend/src/components/ManageReviews/ManageReviews.js
@@ -3,6 +3,7 @@ import { getCurrentUsersReviewsThunk } from "../../store/reviews";
 import ReviewsList from "../ReviewsList/ReviewsList"
 import "./ManageReviews.css";
 import { useEffect } from "react";
+import { NavLink } from 'react-router-dom';
 
 const ManageReviews = () => {
     const reviewsObj = useSelector(state => state.reviews.user);
@@ -19,7 +20,11 @@ const ManageReviews = () => {
     return (
         <div className="manage-reviews-div">
             <h1>Manage Reviews</h1>
-            {reviewsArr.length === 0 ? <p>You have not posted any reviews yet!</p>
+            {reviewsArr.length === 0 ?
+                <div>
+                    <p>You have not posted any reviews yet!</p>
+                    <NavLink className="back-home" to='/'>Go back to the homepage</NavLink>
+                </div>
             : reviewsArr.map(review => (
                 <ReviewsList
                     review={review}
