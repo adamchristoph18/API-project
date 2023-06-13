5c7f2c7f02b33b997afcc05c7d5eed678f7e06c6
Adam Tull <adamchristoph18@gmail.com>
2023-06-13 16:31:35 -0400

    Finished basic styling for the create a new booking modal

--- frontend/src/components/CreateBookingModal/CreateBookingModal.js
+++ frontend/src/components/CreateBookingModal/CreateBookingModal.js
@@ -1,9 +1,39 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useModal } from "../../context/Modal";
 import "./CreateBookingModal.css";
 
 function CreateBookingModal({ spotId, spotObj }) {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { closeModal } = useModal();
+
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+
     return (
         <div>
             <h3 className="book-modal-title">Let's Book Your Stay at {spotObj.name}!</h3>
+            <form>
+                <label className="date-label">Start Date:</label>
+                <input
+                    type="date"
+                    className="top-date-input"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                />
+                <br/>
+                <label className="date-label">End Date:</label>
+                <input
+                    type="date"
+                    className="bottom-date-input"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                />
+                <button type="submit" className="reserve-trip-btn clickable">
+                    Reserve</button>
+            </form>
         </div>
     )
 }
