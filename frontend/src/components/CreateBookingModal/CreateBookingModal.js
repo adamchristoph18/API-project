922a47ad4be745ba234df6731cb90e492d054ed0
Adam Tull <adamchristoph18@gmail.com>
2023-06-13 17:52:48 -0400

    Finished setting up handle submit function for creating a new booking

--- frontend/src/components/CreateBookingModal/CreateBookingModal.js
+++ frontend/src/components/CreateBookingModal/CreateBookingModal.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useModal } from "../../context/Modal";
+import { createNewBookingThunk,
+         getCurrentUsersBookingsThunk } from "../../store/bookings";
 import "./CreateBookingModal.css";
 
 function CreateBookingModal({ spotId, spotObj }) {
@@ -11,11 +13,28 @@ function CreateBookingModal({ spotId, spotObj }) {
 
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
+    const [submitted, setSubmitted] = useState(false);
+    const [errors, setErrors] = useState({});
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setSubmitted(true);
+
+        const payload = { spotId, startDate, endDate };
+        const newBooking = await dispatch(createNewBookingThunk(payload));
+
+        if (newBooking.errors) {
+            setErrors(newBooking.errors);
+        } else {
+            dispatch(getCurrentUsersBookingsThunk());
+            closeModal();
+        }
+    };
 
     return (
         <div>
             <h3 className="book-modal-title">Let's Book Your Stay at {spotObj.name}!</h3>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label className="date-label">Start Date:</label>
                 <input
                     type="date"
