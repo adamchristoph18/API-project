7981b855de2f0705b07c9e0f3011dd01adbbce4e
Adam Tull <adamchristoph18@gmail.com>
2023-06-13 22:22:14 -0400

    Added a line on each booking card for the spots name and city state

--- frontend/src/components/ManageBookings/BookingCard.js
+++ frontend/src/components/ManageBookings/BookingCard.js
@@ -4,6 +4,8 @@ const BookingCard = ({ booking }) => {
     return (
         <div className="booking-card">
             <img className="booking-card-image" src={booking.Spot.previewImage} alt="" />
+            <h5 className="booking-spot-name">{booking.Spot.name}</h5>
+            <p className="booking-city-state">{booking.Spot.city}, {booking.Spot.state}</p>
         </div>
     )
 };
