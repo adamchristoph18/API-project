2aef9325aaa4b4861e8cea1fbc999afb96482bd1
Adam Tull <adamchristoph18@gmail.com>
2023-06-14 01:24:36 -0400

    Added a quick redirect so that when a user clicks on one of the booking cards from the manage bookings page, they get sent to the respective spots show page

--- frontend/src/components/ManageBookings/BookingCard.js
+++ frontend/src/components/ManageBookings/BookingCard.js
@@ -2,17 +2,22 @@ import OpenCreateBookingModal from "../CreateBookingModal/OpenCreateBookingModal
 import CreateBookingModal from "../CreateBookingModal/CreateBookingModal";
 import OpenDeleteBookingModal from "../DeleteBookingModal/OpenDeleteBookingModal";
 import DeleteBookingModal from "../DeleteBookingModal/DeleteBookingModal";
+import { useHistory } from "react-router-dom";
 import "./ManageBookings.css";
 
 const BookingCard = ({ booking }) => {
+
+    const history = useHistory();
 
     const formatDate = (str) => {
         str = str.split("T")[0].split("-");
         return [str[1], str[2], str[0]].join("/");
     };
 
+    const redirect = () => history.push(`/spots/${booking.Spot?.id}`);
+
     return (
-        <div className="booking-card clickable">
+        <div className="booking-card clickable" onClick={redirect}>
             <img className="booking-card-image" src={booking.Spot?.previewImage} alt="" />
             <h5 className="booking-spot-name">{booking.Spot?.name}</h5>
             <p className="booking-city-state">{booking.Spot?.city}, {booking.Spot?.state}</p>
