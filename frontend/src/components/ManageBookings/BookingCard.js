d52e52893ecb801d1412506dcd4f1062ad2ea3a6
Adam Tull <adamchristoph18@gmail.com>
2023-06-13 22:28:57 -0400

    Added a helper function to my booking card component so that each date is formatted properly

--- frontend/src/components/ManageBookings/BookingCard.js
+++ frontend/src/components/ManageBookings/BookingCard.js
@@ -1,11 +1,17 @@
 import "./ManageBookings.css";
 
 const BookingCard = ({ booking }) => {
+    const formatDate = (str) => {
+        str = str.split("T")[0].split("-");
+        return [str[1], str[2], str[0]].join("/");
+    };
+
     return (
         <div className="booking-card">
             <img className="booking-card-image" src={booking.Spot.previewImage} alt="" />
             <h5 className="booking-spot-name">{booking.Spot.name}</h5>
             <p className="booking-city-state">{booking.Spot.city}, {booking.Spot.state}</p>
+            <p>{formatDate(booking.startDate)}</p>
         </div>
     )
 };
