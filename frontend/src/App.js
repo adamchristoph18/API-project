96e209ae5f9c2ae1ae39a1cc2e92297f18cb22a8
Adam Tull <adamchristoph18@gmail.com>
2023-06-15 14:20:03 -0400

    Created, styled a footer component and imported it to App js file so it renders site wide

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
+import Footer from "./components/Footer/Footer";
 import AllSpots from "./components/AllSpots";
 import SpotShow from "./components/SpotShow/SpotShow";
 import ManageSpots from "./components/ManageSpots/ManageSpots";
@@ -22,16 +23,17 @@ function App() {
   return (
     <>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && <Switch>
-          <Route exact path="/"><AllSpots /></Route>
-          <Route path="/spots/new"><CreateNewSpotForm /></Route>
-          <Route path="/spots/current"><ManageSpots /></Route>
-          <Route path="/spots/:spotId/edit"><UpdateSpotForm /></Route>
-          <Route path="/spots/:spotId"><SpotShow /></Route>
-          <Route path="/reviews/current"><ManageReviews /></Route>
-          <Route path="/bookings/current"><ManageBookings /></Route>
-          <p>Page Not Found</p>
-        </Switch>}
+        {isLoaded && <Switch>
+            <Route exact path="/"><AllSpots /></Route>
+            <Route path="/spots/new"><CreateNewSpotForm /></Route>
+            <Route path="/spots/current"><ManageSpots /></Route>
+            <Route path="/spots/:spotId/edit"><UpdateSpotForm /></Route>
+            <Route path="/spots/:spotId"><SpotShow /></Route>
+            <Route path="/reviews/current"><ManageReviews /></Route>
+            <Route path="/bookings/current"><ManageBookings /></Route>
+            <p>Page Not Found</p>
+          </Switch>}
+        <Footer />
     </>
   );
 }
