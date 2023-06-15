79a8b2cd8b6e129ae7dce6b91289e4257741c23e
Adam Tull <adamchristoph18@gmail.com>
2023-06-15 13:40:18 -0400

    Set up the loading page component and imported the beat loader from the react-spinners package

--- frontend/src/components/LoadingPage/LoadingPage.js
+++ frontend/src/components/LoadingPage/LoadingPage.js
@@ -1,1 +1,16 @@
+import BeatLoader from "react-spinners/BeatLoader";
 import "./LoadingPage.css";
+
+function LoadingPage() {
+    return (
+        <div className="loading-page">
+            <BeatLoader
+                loading={true}
+                color="#36d7b7"
+                size={150}
+                />
+        </div>
+    )
+}
+
+export default LoadingPage;
