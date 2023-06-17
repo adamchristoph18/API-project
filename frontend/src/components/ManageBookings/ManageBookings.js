0ba4264695631b90f1dfeee3006b96f188b78a30
Adam Tull <adamchristoph18@gmail.com>
2023-06-16 23:54:07 -0400

    Added a NavLink and the same return to homepage button on the manage bookings page as on the manage reviews page if the user doesn't have any

--- frontend/src/components/ManageBookings/ManageBookings.js
+++ frontend/src/components/ManageBookings/ManageBookings.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { getCurrentUsersBookingsThunk } from "../../store/bookings";
 import BookingCard from "./BookingCard";
@@ -9,6 +9,8 @@ import "./ManageBookings.css";
 const ManageBookings = () => {
     const bookingsObj = useSelector(state => state.bookings.user);
     const bookings = Object.values(bookingsObj).reverse();
+
+    const history = useHistory();
 
     const dispatch = useDispatch();
 
@@ -26,7 +28,11 @@ const ManageBookings = () => {
         <div className="manage-bookings-div">
             <h1>Manage Your Bookings</h1>
             <div className="your-bookings-div">
-                {bookings.length === 0 ? <p>You have no trips scheduled.</p> :
+                {bookings.length === 0 ? <div>
+                    <p>You have no trips scheduled.</p>
+                    <NavLink className="back-home" to='/'>Go back to the homepage</NavLink>
+                    </div>
+                    :
                     bookings.map(booking => (
                         <BookingCard
                             booking={booking}
